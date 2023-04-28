8b9e9d5b52c5490f102471cdb088c9ea07f9ad67
Luke Cooper <luke6.cooper@live.uwe.ac.uk>
2023-04-28 10:59:50 +0100

    New Platforms & changing level design

--- Memory Leak/Assets/Scripts/Pickups/MoveLoop.cs
+++ Memory Leak/Assets/Scripts/Pickups/MoveLoop.cs
@@ -20,7 +20,7 @@ public class MoveLoop : MonoBehaviour
         if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName ("DemoLevel"))
         {
             float y = Mathf.PingPong(Time.time * speed, 0.5f);
-            powerup.transform.localPosition = new Vector3(0, y, 0);
+            powerup.transform.localPosition = new Vector3(1.61f, y, -1.17f);
         }
 
         else if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName ("DemoLvl2"))
